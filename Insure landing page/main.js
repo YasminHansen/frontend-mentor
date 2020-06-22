86ac3b35e1dee5b533ad62c7535500da3cc235fc
Yasmin Hansen <yaa-h@hotmail.com>
2020-06-22 12:33:29 -0300

    Javascript e css do menu mobile

--- Insure landing page/main.js
+++ Insure landing page/main.js
@@ -2,7 +2,7 @@ var windowWidth = window.innerWidth;
 
 if(windowWidth < 992){
     document.getElementById("image-mobile").classList.add('active');
-    document.getElementById("menu-mobile").classList.add('active');
+    document.getElementById("menu-mobile-buttons").classList.add('active');
 }
 else{
     document.getElementById("image-desktop").classList.add('active');
@@ -10,12 +10,14 @@ else{
 }
 
 function openMenuMobile(){
-    document.getElementById("menu-mobile-options").classList.add('active');
+    document.getElementById("menu-mobile").classList.add('active');
     document.getElementById("close-menu-mobile").classList.add('active');
+    document.getElementById("body").classList.add("block-scroll");
     document.getElementById("open-menu-mobile").style.display = "none";
 }
 function closeMenuMobile(){
-    document.getElementById("menu-mobile-options").classList.remove('active');
+    document.getElementById("menu-mobile").classList.remove('active');
     document.getElementById("close-menu-mobile").classList.remove('active');
+    document.getElementById("body").classList.remove("block-scroll");
     document.getElementById("open-menu-mobile").style.display = "block";
 }
