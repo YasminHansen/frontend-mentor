31047bf2ad8383b3a584a9ab55c0cafd3e9038dc
Yasmin Hansen <yaa-h@hotmail.com>
2020-06-22 12:52:44 -0300

    Estilo do menu desktop

--- Insure landing page/main.js
+++ Insure landing page/main.js
@@ -7,17 +7,18 @@ if(windowWidth < 992){
 else{
     document.getElementById("image-desktop").classList.add('active');
     document.getElementById("menu-desktop").classList.add('active');
+    document.getElementById("content").classList.add('container')
 }
 
 function openMenuMobile(){
     document.getElementById("menu-mobile").classList.add('active');
     document.getElementById("close-menu-mobile").classList.add('active');
-    document.getElementById("body").classList.add("block-scroll");
+    document.getElementById("content").classList.add('block-scroll');
     document.getElementById("open-menu-mobile").style.display = "none";
 }
 function closeMenuMobile(){
     document.getElementById("menu-mobile").classList.remove('active');
     document.getElementById("close-menu-mobile").classList.remove('active');
-    document.getElementById("body").classList.remove("block-scroll");
+    document.getElementById("content").classList.remove('block-scroll');
     document.getElementById("open-menu-mobile").style.display = "block";
 }
